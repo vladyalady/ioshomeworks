a39befe5873c9fec1f77f368d8e4b70f26841f90
Vladislav Barinov <vladybarinov@gmail.com>
2022-03-04 13:30:06 +0300

    add navigation in app

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Vladislav Barinov on 04.03.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
