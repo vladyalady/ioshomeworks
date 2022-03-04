1f585c6b898e177dc4118ace061afc9aa635aa2d
Vladislav Barinov <vladybarinov@gmail.com>
2022-03-04 13:39:23 +0300

    create button and postVC

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -12,18 +12,41 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
-        // Do any additional setup after loading the view.
+        addPostButton()
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func buttonTapped() {
+        let postVC = PostViewController()
+        navigationController?.pushViewController(postVC, animated: true)
+
     }
-    */
 
+    private func addPostButton() {
+
+        let postButton = UIButton()
+        postButton.translatesAutoresizingMaskIntoConstraints = false
+
+        postButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        postButton.setTitle("Check post", for: .normal)
+        postButton.setTitleColor(.white, for: .normal)
+        postButton.backgroundColor = .blue
+
+
+        view.addSubview(postButton)
+
+
+
+        let horizontalCenter = NSLayoutConstraint(item: postButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+
+        let verticalCenter = NSLayoutConstraint(item: postButton, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
+
+        let width = NSLayoutConstraint(item: postButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 100)
+
+        let height = NSLayoutConstraint(item: postButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50)
+
+        let constraints: [NSLayoutConstraint] = [horizontalCenter, verticalCenter, width, height]
+
+        NSLayoutConstraint.activate(constraints)
+    }
 }
